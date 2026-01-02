61a92478dc12ff7f90e4251fd9eeda81214449b5
Darrell Plant <329873+darrell-plant@users.noreply.github.com>
2026-01-02 16:37:24 +0900

    add data and icons

--- scripts/create_icons.sh
+++ scripts/create_icons.sh
@@ -22,3 +22,9 @@ magick assets/icon-512.v3.png -resize 16x16 assets/icon-16.png
 
 # optional but good for legacy browsers: multi-size .ico
 magick assets/icon-512.v3.png -define icon:auto-resize=256,128,64,48,32,16 favicon.ico
+
+# android-icon-512.png
+magick icon-512.v3.png \
+  -background black -gravity center -extent 768x768 \
+  -resize 512x512 \
+  android-icon-512.png
