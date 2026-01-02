d72816640bf32db71a3104f9673fb7db2f9cd1e5
Darrell Plant <329873+darrell-plant@users.noreply.github.com>
2026-01-02 18:19:48 +0900

    bump version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'sc-v44';
+const CACHE = 'sc-v45';
 
 const VERSION_ENDPOINT = 'version.txt';
 const VERSION_PATHNAME = new URL(VERSION_ENDPOINT, self.registration.scope).pathname;
